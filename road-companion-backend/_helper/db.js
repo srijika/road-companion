7eb95584a7ea57cb0dbe9fe3602b85f8c5c8ed74
srijika <71753130+srijika@users.noreply.github.com>
2022-01-05 12:30:25 +0530

    Update db.js

--- road-companion-backend/_helper/db.js
+++ road-companion-backend/_helper/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 
-const localDB = "mongodb://127.0.0.1:27017/choovoo";
+const localDB = "mongodb://127.0.0.1:27017/road-companion";
 
 mongoose.connect(process.env.MONGODB_URI || localDB,
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }
